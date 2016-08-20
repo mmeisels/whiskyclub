8b506387789b25d0604ea6c6d92ce12571ad4f81
Michael Meisels <mmeisels@salesforce.com>
2016-08-21 07:08:00 +1000

    Changed Project

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider 'test.salesforce.com', Rails.application.config.salesforce_app_id, Rails.application.config.salesforce_app_secret
+  provider :salesforce, Rails.application.config.salesforce_app_id, Rails.application.config.salesforce_app_secret
 end
