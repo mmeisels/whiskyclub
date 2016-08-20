9535630cc2ef82426c1b8cbbcdc4562c81f83868
Michael Meisels <mmeisels@salesforce.com>
2016-08-20 21:53:00 +1000

    Changed Project

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 FullcalendarAndRailsExample::Application.routes.draw do
-  root 'events#index'
-  resources :events
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+
+  root 'events#index'
+  resources :events
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
