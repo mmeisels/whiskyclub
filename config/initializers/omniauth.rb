a6147e39de8bbb0b69c8ba115617d1c09d908c0e
Michael Meisels <mmeisels@salesforce.com>
2016-08-21 21:41:42 +1000

    Fixed Model Migration

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,5 +7,5 @@ module OmniAuth
 end
 Rails.application.config.middleware.use OmniAuth::Builder do
   ##provider :salesforce, ENV['SALESFORCE_KEY'], ENV['SALESFORCE_SECRET']
-  provider :HoneyBadger, Rails.application.config.salesforce_app_id, Rails.application.config.salesforce_app_secret
+  provider :HoneyBadger, Rails.application.config.salesforce_app_id, Rails.application.config.salesforce_app_secret, 'https://whisky-pr-5.herokuapp.com/oauth/salesforce'
 end
