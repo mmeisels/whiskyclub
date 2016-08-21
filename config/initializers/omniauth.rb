fbc15e11a73739c33a8ca02c62daae9105f90c8f
Michael Meisels <mmeisels@salesforce.com>
2016-08-21 22:24:48 +1000

    Fixed Model Migration

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,5 @@
 OmniAuth.config.logger = Rails.logger
-module OmniAuth
-  module Strategies
-    # tell OmniAuth to load our strategy
-    autoload :HoneyBadger, Rails.root.join('lib','strategies', 'HoneyBadger')
-  end
-end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  ##provider :salesforce, ENV['SALESFORCE_KEY'], ENV['SALESFORCE_SECRET']
-  provider :HoneyBadger
+  provider :salesforce, ENV['SALESFORCE_KEY'], ENV['SALESFORCE_SECRET']
 end
