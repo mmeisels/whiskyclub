1524f6c835d6e9fea19eaa42478686716847e45e
Michael Meisels <mmeisels@salesforce.com>
2016-08-21 21:23:23 +1000

    Fixed Model Migration

--- lib/strategies/HoneyBadger.rb
+++ lib/strategies/HoneyBadger.rb
@@ -4,7 +4,7 @@ require 'base64'
 
 module OmniAuth
   module Strategies
-    class HoneyBadger include OmniAuth::Strategies
+    class HoneyBadger < OmniAuth::Strategies
 
       MOBILE_USER_AGENTS =  'webos|ipod|iphone|ipad|android|blackberry|mobile'
 
